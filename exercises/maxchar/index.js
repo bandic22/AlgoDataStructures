987003cff0d3b532ee086746e55e4178b52b1a2b
Duran Gradwell (HCL America Inc) <v-dugra@microsoft.com>
2018-12-06 15:19:17 -0800

    1-8 done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var maxVal = "";
+    var max = 0;
+    var charMap = {};
+
+    for (var i = 0; i < str.length; i++) {
+        if (!charMap[str[i]]) {
+            charMap[str[i]] = 1;
+        }
+        else {
+            charMap[str[i]]++;
+        }
+
+        if (charMap[str[i]] > max) {
+            max = charMap[str[i]];
+            maxVal = str[i];
+        }
+    }
+
+    return maxVal.toString();
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    var numStr = n.toString();
+    var reversed = "";
+    var isNeg = false;
+
+    if (Math.sign(n) == -1) {
+        isNeg = true;
+        numStr.split('-');
+    }
+
+    for (var i = numStr.length - 1; i >= 0; i--) {
+        if (numStr[i] == "0" && numStr[i+1] == "") {
+            numStr[i] = "";
+        }
+
+        reversed += numStr[i];
+    }
+
+    if (isNeg) {
+        reversed = "-" + reversed;
+    }
+
+    return reversed = parseInt(reversed);
+}
 
 module.exports = reverseInt;
